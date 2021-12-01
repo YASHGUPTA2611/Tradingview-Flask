c272c44d49cb3d252f2069e0fe4adcf9a56bf4e9
Yash Gupta <74678250+YASHGUPTA2611@users.noreply.github.com>
2021-12-01 22:13:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import json
-from flask import Flask, request
+from actions import parse_webhook
+from flask import Flask, request, abort
 
-
+# Create Flask object called app.
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'hello world'
-    print('TMKC')
 
-@app.route('/webhooks',  methods=['POST'])
-def webhooks():
-    data = json.load(request.data)
-    
-    print(data)
+# Create root to easily let us know its on/working.
+@app.route('/')
+def root():
+    return 'online'
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        # Parse the string data from tradingview into a python dict
+        data = parse_webhook(request.get_data(as_text=True))
+        
+if __name__ == '__main__':
+    app.run()
            
