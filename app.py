68b9fd96fbaf0191b70de3b28ef4ce93eae8272b
Yash Gupta <74678250+YASHGUPTA2611@users.noreply.github.com>
2021-12-01 14:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def hello_world():
     return 'hello world'
 
 @app.route('/webhooks',  methods=['POST'])
-def webhook():
-    if request.method=='POST':
-        data = parse_webhook(request.get_data(as_text=True))
-        Print("Alert Received")
+def webhooks():
+    data = json.load(request.data)
+    
+    print(data)
            
