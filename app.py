6792c69aacf56efb8cefcbe651966151325f14c4
Yash Gupta <74678250+YASHGUPTA2611@users.noreply.github.com>
2021-12-01 12:26:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, request,abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+@app.route('/webhooks', methods = ['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return 'success', 200
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
